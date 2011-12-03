bd39f2bde05e40b1aa98027e3f537dbe8b36f6e9
Haynes Mathew George <hgeorge@codeaurora.org>
2011-12-02 21:16:38 -0800

    libstagefright: Enable metadata mode recording
    - Metada mode video recording is enabled by default.
    - use setprop debug.camcorder.disablemeta 1 to disable metadata mode recording.
    
    Change-Id: If994c468c3e24ae03b14462f2311504c3d957426

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1427,10 +1427,17 @@ status_t StagefrightRecorder::setupCameraSource(
                 mTimeBetweenTimeLapseFrameCaptureUs);
         *cameraSource = mCameraSourceTimeLapse;
     } else {
+
+        bool useMeta = true;
+        char value[PROPERTY_VALUE_MAX];
+        if (property_get("debug.camcorder.disablemeta", value, NULL) &&
+            atoi(value)) {
+            useMeta = false;
+        }
         *cameraSource = CameraSource::CreateFromCamera(
                 mCamera, mCameraProxy, mCameraId, videoSize, mFrameRate,
 #ifdef QCOM_HARDWARE
-                mPreviewSurface, false);
+                mPreviewSurface, useMeta);
 #else
                 mPreviewSurface, true /*storeMetaDataInVideoBuffers*/);
 #endif
@@ -1604,6 +1611,7 @@ status_t StagefrightRecorder::setupVideoEncoder(
 
     uint32_t encoder_flags = 0;
     if (mIsMetaDataStoredInVideoBuffers) {
+        LOGW("Camera source supports metadata mode, create OMXCodec for metadata");
         encoder_flags |= OMXCodec::kHardwareCodecsOnly;
         encoder_flags |= OMXCodec::kStoreMetaDataInVideoBuffers;
     }
