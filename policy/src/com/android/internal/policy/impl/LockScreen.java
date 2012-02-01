57ea2c1a521b6ec6af37565c95d61fcc6ce4a591
Nico Kaiser <nk111@gmx.de>
2012-02-01 14:54:19 +0000

    Fix don't interrupt ongoing PIN/PUK verification
    This fixes an issue which only occurs if
    1. you use a SIM PIN
    2. the PIN verification takes longer than the screen timeout is. e.g. when then screen turns black while "unlocking SIM" message is displayed!
    
    It might be possible to simulate the bug by pressing power quickly after the PIN has ben entered and "unlocking SIM" message is still being displayed.
    
    If the PIN verification takes longer thant the screen timeout is then the screen turns black.
    If that happens LockPatternKeyguardView.reset() is called which then forces a re-creation of the view. But then the original created
    SimUnlockScreen.onSimLockChangedResponse callback never receives the success message for the SIM unlock.
    What then happens is that the LockPatternKeyguardView is re-created again and again whenever screen turns off while waiting
    until we receive a SIM state changed event e.g. until we are back on network.
    But when that happens we are presented the pattern unlock screen. With a single touch we can then (finally) get on the homescreen.
    
    The fix removes the forced re-creation of the LockPatternKeyguardView if there is a PIN/PUK verification in progress. Which leads to a 'normal' unlock behavior:
    1. enter pin
    2. "unlocking SIM" message is displayed
    3. maybe screen turns off after timeout
    4. after some seconds you get on the homescreen directly
    
    Change-Id: Ib3080a8844a36e27226f6d5e6ca4f50d79364c75

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -703,6 +703,11 @@ class LockScreen extends LinearLayout implements KeyguardScreen {
     }
 
     /** {@inheritDoc} */
+    public boolean checkInProgress() {
+        return false;
+    }
+
+    /** {@inheritDoc} */
     public boolean needsInput() {
         return false;
     }
