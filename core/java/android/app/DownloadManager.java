2fe8fcd8f448f75cf5f7823f91586ea627d20c0a
Håkan3 Johansson <hakan3.johansson@sonyericsson.com>
2012-02-08 21:13:35 +0100

    Adds missing Bluetooth as network type.
    The network connection type for Bluetooth is added so
    that it is possible to use Bluetooth as Hotspot during
    download from the Internet.
    
    Change-Id: If8e8a3f69821beef742bc91d7a292a091861b48b
    
    Conflicts:
    
    	core/java/android/app/DownloadManager.java

--- core/java/android/app/DownloadManager.java
+++ core/java/android/app/DownloadManager.java
@@ -348,6 +348,13 @@ public class DownloadManager {
          */
         public static final int NETWORK_WIMAX = 1 << 6;
 
+        /**
+         * Bit flag for {@link #setAllowedNetworkTypes} corresponding to
+         * {@link ConnectivityManager#TYPE_BLUETOOTH}.
+         * @hide
+         */
+        public static final int NETWORK_BLUETOOTH = 1 << 2;
+
         private Uri mUri;
         private Uri mDestinationUri;
         private List<Pair<String, String>> mRequestHeaders = new ArrayList<Pair<String, String>>();
