4a0cbebed7f4dfe280ef2fbbcf1d107024966e6b
Robin Morawetz <robin.morawetz@googlemail.com>
2012-02-11 18:34:42 +0100

    Make national data roaming optional (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2279,6 +2279,11 @@ public final class Settings {
 	 */
 	public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
+     /**
+      * Whether national data roaming should be used.
+      */
+     public static final String MVNO_ROAMING = "mvno_roaming";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
