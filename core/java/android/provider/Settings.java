b340699d31b3388ad9cc7e706d016bf03b8d0f5b
Davor Bertovic <davor@losinj.com>
2012-02-15 23:37:03 +0100

    Configurable 0, 90, 180 and 270 degree rotation (framework part)
    This is similar to my previous CM contributions. Requires settings
    UI to configure (not done yet). The rationale is to be able to
    have automatic rotation always enabled, but only for desired
    angles.
    
    Change-Id: I19d3d2cb91249ba2cdee310e6ff7b0978e9bfde0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2279,10 +2279,24 @@ public final class Settings {
 	 */
 	public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
-     /**
-      * Whether national data roaming should be used.
-      */
-     public static final String MVNO_ROAMING = "mvno_roaming";
+        /**
+         * Whether national data roaming should be used.
+         */
+        public static final String MVNO_ROAMING = "mvno_roaming";
+
+	/**
+         * Control the type of rotation which can be performed using the accelerometer
+         * if ACCELEROMETER_ROTATION is enabled.
+         * Value is a bitwise combination of
+         * 1 = 0 degrees (portrait)
+         * 2 = 90 degrees (left)
+         * 4 = 180 degrees (inverted portrait)
+         * 8 = 270 degrees (right)
+         * Setting to 0 is effectively orientation lock
+         * @hide
+         */
+        public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
+
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
