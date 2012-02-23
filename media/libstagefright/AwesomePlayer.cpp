eac8792bfee43ff21bf0f099ee47a6a8b09bd366
Vikash Garodia <vgarodia@codeaurora.org>
2012-02-23 15:39:34 +0530

    libstagefright:FIRST_FRAME is set with audioEOS event
    As audioEOS event is posted, timesource is changed from audio clock to
    system timesource.Because of the difference in two timeclock,it assumes it
    to be late/early and leads to seek.
    By setting the FIRST_FRAME with audioEOS event,the timesource difference is
    taken into account while lateness calculation.
    
    CRs-Fixed: 335557
    
    (cherry picked from commit 7c75a90eca414b7424d1b1fc2bb55642dd46c317)
    
    Change-Id: I504eac51d42bf78e42fdd453d8d5068ea7d81ca5

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -2021,7 +2021,6 @@ void AwesomePlayer::onCheckAudioStatus() {
     if (mWatchForAudioEOS && mAudioPlayer->reachedEOS(&finalStatus)) {
         mWatchForAudioEOS = false;
         modifyFlags(AUDIO_AT_EOS, SET);
-        modifyFlags(FIRST_FRAME, SET);
         postStreamDoneEvent_l(finalStatus);
     }
 }
@@ -2379,6 +2378,7 @@ uint32_t AwesomePlayer::flags() const {
 
 void AwesomePlayer::postAudioEOS(int64_t delayUs) {
     mAudioEOD = true;
+    modifyFlags(FIRST_FRAME, SET);
     postCheckAudioStatusEvent(delayUs);
 }
 
