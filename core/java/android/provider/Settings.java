7b4cd28324fd1f5e6eb417caea95bcf9ef62b455
LorD ClockaN <davor@losinj.com>
2012-02-26 08:41:17 +0100

    fix MVNO opition

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1894,6 +1894,11 @@ public final class Settings {
          */
         public static final String POINTER_SPEED = "pointer_speed";
 
+	/**
+         * Whether national data Roming should be used.
+         */
+        public static final String MVNO_ROAMING = "mvno_roaming";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
@@ -3673,11 +3678,6 @@ public final class Settings {
          * @hide
          */
         public static final String CDMA_ROAMING_MODE = "roaming_settings";
-
-        /**
-         * Whether national data Roming should be used.
-         */
-        public static final String MVNO_ROAMING = "mvno_roaming";
 
         /**
          * The CDMA subscription mode 0 = RUIM/SIM (default)
