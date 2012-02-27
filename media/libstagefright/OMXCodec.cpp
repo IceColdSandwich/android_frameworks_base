4c56d082105c384855d2bc6d909d24062a3c4e13
Haynes Mathew George <hgeorge@codeaurora.org>
2012-02-27 15:06:02 -0500

    libstagefright: Fix to free buffers properly in ERROR state

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -2954,6 +2954,11 @@ status_t OMXCodec::freeBuffer(OMX_U32 portIndex, size_t bufIndex) {
     if (err == OK) {
         buffers->removeAt(bufIndex);
     }
+    else {
+        LOGW("Warning, free Buffer failed, to be freed later"
+             " returning OK to prevent crash..");
+        return OK;
+    }
 
     return err;
 }
@@ -3870,6 +3875,10 @@ status_t OMXCodec::stop() {
 
             if (state != OMX_StateExecuting) {
                 break;
+            }
+            else {
+                CODEC_LOGV("Component is still in executing state, fall through and move component"
+                           " to idle");
             }
             // else fall through to the idling code
             isError = true;
