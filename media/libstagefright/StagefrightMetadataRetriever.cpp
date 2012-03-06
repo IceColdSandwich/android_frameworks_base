be1ec4da0e718ac0f0b9da61368e738159e7df64
Phani Kumar Allada <pallad@codeaurora.org>
2012-03-06 12:41:19 -0500

    frameworks/base: Fix for AAC clip with 3gp extension not detected as audio file
    - Clip which has AAC audio data but with .3gp extension is not
      detected as audio file and shown as video file.
    
    - MIMETYPE for audio only 3GPP is set to video/3gpp and hence shown
      as video clip.
    
    - MIMETYPE for AAC audio clip with .3gpp extension is set to audio/aac
      to detect it as audio file.
    
    CRs-Fixed: 337692
    (cherry picked from commit dca241864a8af8c9edfa4b62f93c2052d5fca40a)
    
    Change-Id: Icbb9b9ec9cb231be2208be2c2ceb12700f49d83d

--- media/libstagefright/StagefrightMetadataRetriever.cpp
+++ media/libstagefright/StagefrightMetadataRetriever.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2011-2012, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -642,6 +643,19 @@ void StagefrightMetadataRetriever::parseMetaData() {
             }
         }
 #endif
+        // Allow Audio only 3gp clips to be considered as audio clips
+        if (!strcasecmp(fileMIME, "video/3gpp") ||
+                !strcasecmp(fileMIME, "audio/mp4a-latm")) {
+            sp<MetaData> trackMeta = mExtractor->getTrackMetaData(0);
+            const char *trackMIME;
+            CHECK(trackMeta->findCString(kKeyMIMEType, &trackMIME));
+
+            if (!strcasecmp("audio/mp4a-latm", trackMIME)) {
+                mMetaData.add(
+                        METADATA_KEY_MIMETYPE, String8("audio/aac"));
+            }
+
+        }
     }
 
     // To check whether the media file is drm-protected
