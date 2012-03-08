48749ec79e0af35a4ec39e23b3943bdf9ed58515
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-03-08 16:56:17 +0100

    Panorama: always allow read/write on buffers

--- libs/ui/GraphicBufferAllocator.cpp
+++ libs/ui/GraphicBufferAllocator.cpp
@@ -103,6 +103,12 @@ status_t GraphicBufferAllocator::alloc(uint32_t w, uint32_t h, PixelFormat forma
     if (format == HAL_PIXEL_FORMAT_YV12) {
 	format = HAL_PIXEL_FORMAT_RGBX_8888;
     }
+    if (!(usage & GRALLOC_USAGE_SW_READ_MASK)) {
+        usage |= GRALLOC_USAGE_SW_READ_RARELY;
+    }
+    if (!(usage & GRALLOC_USAGE_SW_WRITE_MASK)) {
+        usage |= GRALLOC_USAGE_SW_WRITE_RARELY;
+    }
     if (usage & GRALLOC_USAGE_EXTERNAL_DISP) {
 	usage ^= GRALLOC_USAGE_EXTERNAL_DISP;
     }
