fb95de7c0029f1d77698001fff5dbb7b70397e04
LorD ClockaN <davor@losinj.com>
2012-03-09 16:22:59 +0100

    REVERT new libpng update

--- cmds/screenshot/Android.mk
+++ cmds/screenshot/Android.mk
@@ -7,6 +7,6 @@ LOCAL_MODULE := screenshot
 
 LOCAL_SHARED_LIBRARIES := libcutils libz
 LOCAL_STATIC_LIBRARIES := libpng
-LOCAL_C_INCLUDES += external/zlib external/libpng
+LOCAL_C_INCLUDES += external/zlib
 
 include $(BUILD_EXECUTABLE)
