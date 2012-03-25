5fee1d533b56c8ba0c12f6242d349f66502dc121
sethyx <sethyx@gmail.com>
2012-03-25 13:10:00 +0200

    also track calendar changes on lockscreen

--- policy/src/com/android/internal/policy/impl/LockPatternKeyguardView.java
+++ policy/src/com/android/internal/policy/impl/LockPatternKeyguardView.java
@@ -736,6 +736,9 @@ public class LockPatternKeyguardView extends KeyguardViewBase implements Handler
     public void onRefreshWeatherInfo(Intent weatherIntent)
     {}
     @Override
+    public void onRefreshCalendarInfo()
+    {}
+    @Override
     public void onTimeChanged() {}
     @Override
     public void onRefreshCarrierInfo(CharSequence plmn, CharSequence spn) {}
