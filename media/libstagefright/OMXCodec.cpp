e1a61d0b2e9604c62e55e30ab7d22bb76c3f944f
Haynes Mathew George <hgeorge@codeaurora.org>
2012-04-25 11:27:03 -0700

    libstagefright: Free all video decoder output buffers on an error
    - OMXCodec calls dequeueBuffer on the NativeWindow in a loop
      and registers output buffers with the OMX component
    - If a dequeueBuffer fails, the buffers already registered
      with the OMX component were not getting freed laeding
      to a memory leak.
    
    Change-Id: I56abc7577b10e7540bdd6488708ae6830b9d907b
    CRs-Fixed: 351388
    (cherry picked from commit 0d4e06d9a53240e0e7aca05a64faea353babb029)

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -2725,9 +2725,13 @@ status_t OMXCodec::allocateOutputBuffersFromNativeWindow() {
         cancelEnd = def.nBufferCountActual;
     }
 
-    for (OMX_U32 i = cancelStart; i < cancelEnd; i++) {
-        BufferInfo *info = &mPortBuffers[kPortIndexOutput].editItemAt(i);
-        cancelBufferToNativeWindow(info);
+    if (err != 0) {
+        freeBuffersOnPort(kPortIndexOutput);
+    } else {
+        for (OMX_U32 i = cancelStart; i < cancelEnd; i++) {
+            BufferInfo *info = &mPortBuffers[kPortIndexOutput].editItemAt(i);
+            cancelBufferToNativeWindow(info);
+        }
     }
 
     return err;
