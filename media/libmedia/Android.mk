9da95244f1d3c2f142f12a45d195693cff92fb79
Daniel Bateman <jetison.24@gmail.com>
2012-04-27 13:24:12 -0400

    libmedia: don't require USES_AUDIO_LEGACY for USE_KINETO_COMPATIBILITY
    squashed:
    audio: Add compatibility bits to use legacy libaudio implementations
    
    Change-Id: I25b6fce494f6cff111b20611dd788025cebfe818

--- media/libmedia/Android.mk
+++ media/libmedia/Android.mk
@@ -56,10 +56,22 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     LOCAL_CFLAGS += -DQCOM_HARDWARE
 endif
 
+ifeq ($(BOARD_USES_AUDIO_LEGACY),true)
+    LOCAL_SRC_FILES+= \
+        AudioParameter.cpp
+
+    LOCAL_CFLAGS += -DUSES_AUDIO_LEGACY
+endif
+
+ifeq ($(BOARD_USE_KINETO_COMPATIBILITY),true)
+    LOCAL_CFLAGS += -DUSE_KINETO_COMPATIBILITY
+endif
+
 LOCAL_SHARED_LIBRARIES := \
-	libui libcutils libutils libbinder libsonivox libicuuc libexpat \
-        libcamera_client libstagefright_foundation \
-        libgui libdl
+    libui libcutils libutils libbinder libsonivox libicuuc libexpat \
+    libcamera_client libstagefright_foundation \
+    libgui libdl
+
 
 LOCAL_WHOLE_STATIC_LIBRARY := libmedia_helper
 
