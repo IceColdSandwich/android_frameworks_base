bc706d21287eaa47b46403bb3efeea2fb1d7446a
Kyong Hwa Bae <kbae@codeaurora.org>
2012-04-27 10:52:56 -0700

    SurfaceFlinger: Set the transform value correctly
    When using the transform value, read only last 4 bits
    of it regarding the change of
    transform value in Layer::setGeometry().
    
    Change-Id: Icdb2c9d8c283867a273e9633a7a563516f6af637
    Depends-on: 120066

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -212,8 +212,8 @@ void HWComposer::dump(String8& result, char* buffer, size_t SIZE,
             }
             snprintf(buffer, SIZE,
                     " %8s | %08x | %08x | %08x | %02x | %05x | %08x | [%5d,%5d,%5d,%5d] | [%5d,%5d,%5d,%5d] %s\n",
-                    l.compositionType ? "OVERLAY" : "FB",
-                    intptr_t(l.handle), l.hints, l.flags, l.transform, l.blending, format,
+                    l.compositionType ? (l.compositionType == HWC_OVERLAY ? "OVERLAY" : "COPYBIT") : "FB",
+                    intptr_t(l.handle), l.hints, l.flags, l.transform & FINAL_TRANSFORM_MASK, l.blending, format,
                     l.sourceCrop.left, l.sourceCrop.top, l.sourceCrop.right, l.sourceCrop.bottom,
                     l.displayFrame.left, l.displayFrame.top, l.displayFrame.right, l.displayFrame.bottom,
                     layer->getName().string());
