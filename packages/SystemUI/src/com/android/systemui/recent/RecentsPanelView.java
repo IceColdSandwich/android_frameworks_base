19a5376e9107ae0811ea2b87c0c4a59df002be3f
Robin Morawetz <robin.morawetz@googlemail.com>
2012-05-07 22:02:06 +0200

    Revert "Add "Kill-all"-button to recent apps"
    This reverts commit cfba7e4e20fa29abfded7ae1ee754917820bda82.

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -43,7 +43,6 @@ import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
@@ -72,7 +71,6 @@ public class RecentsPanelView extends RelativeLayout implements OnItemClickListe
     private View mRecentsScrim;
     private View mRecentsNoApps;
     private ViewGroup mRecentsContainer;
-    private Button killAllButton;
 
     private boolean mShowing;
     private Choreographer mChoreo;
@@ -352,13 +350,6 @@ public class RecentsPanelView extends RelativeLayout implements OnItemClickListe
         super.onFinishInflate();
         mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         mRecentsContainer = (ViewGroup) findViewById(R.id.recents_container);
-        killAllButton = (Button) findViewById(R.id.recents_kill_all_button);
-        killAllButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                killAllRecentApps();
-            }
-        });
         mListAdapter = new TaskDescriptionAdapter(mContext);
         if (mRecentsContainer instanceof RecentsHorizontalScrollView){
             RecentsHorizontalScrollView scrollView
@@ -644,22 +635,5 @@ public class RecentsPanelView extends RelativeLayout implements OnItemClickListe
             }
         });
         popup.show();
-    }
-
-    public void killAllRecentApps(){
-       final ActivityManager am = (ActivityManager)
-                mContext.getSystemService(Context.ACTIVITY_SERVICE);
-        if(!mRecentTaskDescriptions.isEmpty()){
-            for(TaskDescription ad : mRecentTaskDescriptions){
-                am.removeTask(ad.persistentTaskId, ActivityManager.REMOVE_TASK_KILL_PROCESS);
-                // Accessibility feedback
-                setContentDescription(
-                        mContext.getString(R.string.accessibility_recents_item_dismissed, ad.getLabel()));
-                sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_SELECTED);
-                setContentDescription(null);
-            }
-            mRecentTaskDescriptions.clear();
-        }
-        hide(false);
     }
 }
