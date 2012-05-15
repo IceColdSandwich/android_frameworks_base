ad01e4529baecb8627f7109461b6d1bdcb4e1adc
Arvind Thatikonda <arvindt@codeaurora.org>
2012-05-15 15:35:43 +0530

    libstagefright: Fix Loading Video displayed even after rebuffering Issue
    This change fixes the Loading Video Icon being displayed even after
    rebuffering is complete.
    
    It also fixes AV Interleaving Offset calculation which was calculated in
    bits. With this change, we are calculating AV Interleaving offset as
    bytes instead of bits.
    
    CRs-Fixed: 355654
    (cherry picked from commit 90d13f1ed8144cdd89c36ccb61390adcf39522be)
    
    Change-Id: Ic8bb76668e659653b33093d5bd4728d6da13e801

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1335,6 +1335,7 @@ status_t AwesomePlayer::seekTo(int64_t timeUs) {
         return seekTo_l(timeUs);
     } else {
         notifyListener_l(MEDIA_SEEK_COMPLETE);
+        notifyListener_l(MEDIA_INFO, MEDIA_INFO_BUFFERING_END);
         mSeekNotificationSent = true;
     }
     return OK;
@@ -2345,7 +2346,7 @@ void AwesomePlayer::onPrepareAsyncEvent() {
             int64_t bitrate = 0;
             if( getBitrate( &bitrate ) ) {
                 //considering that Audio Video can be apart by 0.3 secs
-                mCachedSource->setAVInterleavingOffset( bitrate/3 );
+                mCachedSource->setAVInterleavingOffset( bitrate/(3 * 8) );
             }
         }
         postBufferingEvent_l();
