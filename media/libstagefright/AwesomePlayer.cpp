a1a2ba4318e3c162156d1293b2e90a4275f7dd1a
Arvind Thatikonda <arvindt@codeaurora.org>
2012-06-26 16:06:20 +0530

    libstagefright: Properly disconnects connecting data source in reset
    - Moving the disconnect part of connecting data source under a
    mutex lock.
    - This prevents ANR in streaming scenarios when we have multiple
    reset calls from the Media Player.
    
    CRs-Fixed: 373589
    (cherry picked from commit 655a7c33ae35ce0c52f7fa96dc4d73f34f808df4)
    
    Change-Id: I811501d3409b53b4f3a0334c82c24dcfdf0c6f5f

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -496,11 +496,11 @@ status_t AwesomePlayer::setDataSource_l(const sp<MediaExtractor> &extractor) {
 }
 
 void AwesomePlayer::reset() {
+    Mutex::Autolock autoLock(mLock);
     //Disconnect datasource
     if (mConnectingDataSource != NULL) {
         mConnectingDataSource->disconnect();
     }
-    Mutex::Autolock autoLock(mLock);
     reset_l();
 }
 
