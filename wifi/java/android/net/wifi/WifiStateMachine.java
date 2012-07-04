324bd4547e18c740d29f0688a1d9e95422c7f82d
Emerson Pinter <dev@pinter.com.br>
2012-07-04 20:13:20 -0300

    frameworks/base: Fix wifi-sleep-policy
    Without this patch, the wifi sleep policy don't stop. Wifi
    keeps turned on after the screen is off, no matter what I choose in
    Settings.
    
    Solves this issue:
    http://code.google.com/p/android/issues/detail?id=23211
    
    Change-Id: I3942827db00bd448827390bbc6ca31673d8e3397

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -2621,8 +2621,8 @@ public class WifiStateMachine extends StateMachine {
                     }
                     mWakeLock.acquire();
                     WifiNative.stopDriverCommand();
-                    transitionTo(mDriverStoppingState);
                     mWakeLock.release();
+                    transitionTo(mDriverStoppingState);
                     break;
                 case CMD_START_PACKET_FILTERING:
                     if (message.arg1 == MULTICAST_V6) {
